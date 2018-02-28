223afd0dcb56a3b4d2b459853523dc7830b32628
maxiom <maxiom@hotmail.com>
2018-02-27 19:59:13 -0800

    changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-console.log('Hello World!')
+console.log('Hello World!');
 var steem = require('steem');
 
 steem.api.setOptions({url: 'https://api.steemit.com'});
 
 steem.api.getAccounts(['maxiom', 'dan'], function(err, res){
   console.log(err, res);
-})
+});
